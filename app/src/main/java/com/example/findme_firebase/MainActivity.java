a70dbb86831124bc831aecd4b510e7a620907711
JiaWeiPls <jlee211@e.ntu.edu.sg>
2020-11-23 01:10:36 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/findme_firebase/MainActivity.java
+++ app/src/main/java/com/example/findme_firebase/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.findme_firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -23,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
+                Toast.makeText(MainActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, StartActivity.class));
             }
         });
     }
